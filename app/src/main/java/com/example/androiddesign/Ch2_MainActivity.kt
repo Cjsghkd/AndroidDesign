341ff358f943a02fd9354fa01d865ee33dd374a6
Cjsghkd <pjhomg5530@naver.com>
2022-08-08 17:36:36 +0900

    데이터바인딩_2

--- app/src/main/java/com/example/androiddesign/Ch2_MainActivity.kt
+++ app/src/main/java/com/example/androiddesign/Ch2_MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androiddesign
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.androiddesign.databinding.ActivityCh2MainBinding
 
 class Ch2_MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding : ActivityCh2MainBinding
+    private val model : Ch2_MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ch2_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_ch2_main)
+
+        mBinding.lifecycleOwner = this
+        mBinding.viewModel = model
     }
 }
